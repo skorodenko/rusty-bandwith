925e2ff990e581dd5bfdf6b25576ddf18fb773e9
skorodenko <mskorodenko@gmail.com>
2026-08-08 08:29:46 +0300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Rust devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-unstable,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+        };
+        overlays = [
+          (import rust-overlay)
+        ];
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [
+              pkgs-unstable.rust-analyzer
+              rust-bin.beta.latest.default
+              pkgs-unstable.sccache
+
+              pkg-config
+              alsa-lib
+              wayland
+              libxkbcommon
+              vulkan-loader
+              libGL
+              udev
+            ];
+
+            shellHook = ''
+              export RUST_LOG=DEBUG
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.libxkbcommon
+                  pkgs.wayland
+                  pkgs.vulkan-loader
+                  pkgs.libGL
+                ]
+              }"
+            '';
+          };
+      }
+    );
+}
